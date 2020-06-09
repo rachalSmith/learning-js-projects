f34c1d095cb603f773c386f8ae3923910611b677
rachal smith <rachalsmith@gmail.com>
2020-06-09 21:28:37 +0100

    Variables practice. Move js practice to GitHub via the command line.

---
+++ kelvinConverter-var.js
@@ -0,0 +1,12 @@
+const kelvin = 288;
+
+const celsius = kelvin -273;
+
+//console.log(`Todays temperature is ${celsius}`) should red 15
+
+const fahrenheit = Math.floor(celsius*(9/5)+32);
+
+//equation to convert degrees into fahrenheit logging should print 59. Rounded to nearest integer
+
+
+console.log(`Today's temperature is ${celsius} degrees or ${fahrenheit} fahrenheit.`);
